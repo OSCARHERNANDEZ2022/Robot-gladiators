67b69ee150f99c97c403194f41f67700c0f0b2fd
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-02-28 19:29:14 -0700

    added JavaScript

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,9 +6,10 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = " Roberto ";
+var enemyNames = [ "Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 10;
+console.log(enemyNames.length);
 
 var fight = function() {
     // Alert players that they are starting the round
@@ -20,20 +21,20 @@ var fight = function() {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+            playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining. "
         );
 
         // check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " has died!");
+            window.alert(enemyNames + " has died!");
         } else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
         }
 
         // remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+            enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
         );
 
         // check player's health
@@ -56,7 +57,11 @@ var fight = function() {
         else {
             fight();
         }
-      
-    }    
-}
+        // if player did not chose 1 or 2 in prompt
+      } else {
+          window.alert("You need to pick a valid option. Try again!");
+      }   
+};
+
+//run fight function to start game
 fight();
